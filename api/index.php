478b6e378871f9214681bf1780461943a4e38b23
misterdhimz <dhimazanswer@gmail.com>
2025-12-15 09:37:18 +0700

    Fix download issue final

--- api/index.php
+++ api/index.php
@@ -1,9 +1,11 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+
 $app = require __DIR__ . '/../bootstrap/app.php';
 
 $request = Illuminate\Http\Request::capture();
 $response = $app->handle($request);
+
 $response->send();
 $app->terminate();
